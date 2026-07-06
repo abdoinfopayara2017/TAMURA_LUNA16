3d85ebd30cbaccb5c758e6ab81004176f8327cd6
ABDERRAZZAK FELIOUNE <ali192010@gmail.com>
2026-07-06 15:26:04 +0100

    adapt to WSL

--- test.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-rows = 5
-cols = 5
-# Create meshgrid based on row/column counts
-r, _ = tf.meshgrid(tf.range(rows),tf.range(cols), indexing='ij')
-print(r)
--- train.py
+++ train.py
@@ -1,54 +1,45 @@
 
 import numpy as np
 import pandas as pd
-import dataprocess.textural_features as txnp
 import dataprocess.textural_features_tensor_flow as txtf 
 import tensorflow as tf
-import tensorflow_probability as tfp
 
-def train(path,truth):
+def train(path):
     
-    '''
-    Preprocessing for dataset
-    '''
-    # Read  data set (Train data from CSV file)
-    # csv file should have the type:
-    # label,data_npy
-    # label,data_npy
-    # ....
-    #
+    with tf.device('/gpu:0'):
+      '''
+      Preprocessing for dataset
+      '''
+      # Read  data set (Train data from CSV file)
+      # csv file should have the type:
+      # label,data_npy
+      # label,data_npy
+      # ....
+      #
 
-    csvimagedata = pd.read_csv(path)
-    data = csvimagedata.iloc[:, :].values
-    np.random.shuffle(data)
-    # For Image
-    images = data[:, 1:]
-    # For Labels
-    labels = data[:, 0]   
-    tab = tf.zeros(0)
-    tabnp = np.zeros(0)
-    for i in range (1):
-     image = np.load(images[i][0])
-     if np.sum(image):
+      csvimagedata = pd.read_csv(path)
+      data = csvimagedata.iloc[:, :].values
+      #np.random.shuffle(data)
+      # For Image
+      images = data[:, 1:]
+      # For Labels
+      labels = data[:, 0]    
+      for index in csvimagedata.index:
+        image = np.load(images[index][0])
+        if np.sum(image):      
+          F_crs = txtf.calc_coarseness(image,5,0.9)
+          csvimagedata.loc[index, "coarseness"] = float(F_crs)             
+          F_cos = txtf.contrast(image)      
+          csvimagedata.loc[index, "contrast"] = float(F_cos)
+          F_dir,*_ = txtf.directionality(image)
+          csvimagedata.loc[index, "directionality"] = float(F_dir)      
+          F_lin_tf = txtf.linelikeness(image)
+          csvimagedata.loc[index, "linelikeness"] = float(F_lin_tf)
+          F_reg = txtf.regularity(image)
+          csvimagedata.loc[index, "regularity"] = float(F_reg)
+          F_rgh= txtf.roughness(image)  
+          csvimagedata.loc[index, "roughness"] = float(F_rgh)      
       
-      #F_crs = txtf
-      #F_crs_np = txnp.calc_coarseness(image,5,0.9)       
-      #F_cos = txtf.contrast(image)
-      #F_cos_np = txnp.contrast(image)      
-      #F_dir,*_ = txtf.directionality(image)
-      #F_dir_np,*_ = txnp.directionality(image)
-      #F_lin = txnp.linelikeness(image)
-      #F_lin_tf = txtf.linelikeness(image)
-      #F_reg = txtf.regularity(image)
-      F_rgh= txtf.roughness(image)
-      
-      tab = tf.concat([tab,F_rgh],0)
-      #tabnp=np.append(tabnp,F_lin)
-      
-    
-    
-    tf.print(tfp.stats.percentile(tab, 50.0, interpolation='midpoint'),tf.reduce_max(tab),tf.reduce_min(tab))
-    #print("Median Max and Min of dataset %d : %2f %2f %2f" %(truth,np.median(tabnp),np.max(tabnp),np.min(tabnp)))
+      csvimagedata.to_csv("data.csv", index=False)    
 
-train('dataprocess\\data\\training_class_0.csv',0)
-#train('dataprocess\\data\\training_class_1.csv',1)
+train('dataprocess/data/training_origine.csv')
